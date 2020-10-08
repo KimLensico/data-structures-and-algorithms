0fb1811bcc265707bfc5ed99940fd7c231d59fbb
KIMBERLY LENSICO <KIMBERLYLENSICO@OUTLOOK.COM>
2020-10-08 16:53:50 -0700

    got 3/4 right

--- code-challenges/challenges-01.test.js
+++ code-challenges/challenges-01.test.js
@@ -13,7 +13,7 @@ const greeting = (word) => {
 };
 
 const speaker = (message, callback) => {
-  return callback.toUpperCase();
+  return callback(message);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -33,12 +33,14 @@ Return the modified array.
 ------------------------------------------------------------------------------------------------ */
 
 function addValues(arr, value) {
-  arr = []
   arr.push(value);
 }
 
 const addNumbers = (num, arr, times, callback) => {
-  // Solution code here...
+  for (let i = 0; i < times; i++) { // incrementing through each value
+    callback(arr, num) // we ran through the times value and now we are using callback to invoke the function after going through the for loop
+  }
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -60,20 +62,26 @@ This function should use forEach to populate your grocery list based on the stor
 ------------------------------------------------------------------------------------------------ */
 
 const createList = (availableItems) => {
-  // Solution code here...
+  let fruits = [];
+  availableItems.forEach(element => { //element = placeholder value
+    if (element.available) { //for each item of the array, it checks if it's available 
+      fruits.push(element.name); //pushed all the available inventory
+    }
+  });
+  return fruits; //returns all the available fruits
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
-
+ 
 Write a function named fizzbuzz that takes in an array of numbers.
-
+ 
 Iterate over the array using forEach to determine the output based on several rules:
   - If a number is divisible by 3, add the word "Fizz" to the output array.
   - If the number is divisible by 5, add the word "Buzz" to the output array.
   - If the number is divisible by both 3 and 5, add the phrase "Fizz Buzz" to the output array.
   - Otherwise, add the number to the output array.
-
+ 
 Return the resulting output array.
 ------------------------------------------------------------------------------------------------ */
 
@@ -83,13 +91,13 @@ const fizzbuzz = (arr) => {
 
 /* ------------------------------------------------------------------------------------------------
 TESTS
-
+ 
 All the code below will verify that your functions are working to solve the challenges.
-
+ 
 DO NOT CHANGE any of the below code.
-
+ 
 Run your tests from the console: jest challenges-01.test.js
-
+ 
 ------------------------------------------------------------------------------------------------ */
 
 describe('Testing challenge 1', () => {
